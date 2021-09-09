e7f52d397c4b3102b38fe5d836ac3c169bfa4a25
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-09-09 14:46:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+// const record = [
+//     {year: "2018", result: "L"},
+//     {year: "2017", result: "W"},
+//     {year: "2016", result: "N/A"}
+//   ]
 
+  function superbowlWin(record) {
+      let win = record.find(r => r.result === "W")
+
+      if(win === true) {
+          return win.year
+      } else {
+          undefined
+      }
+  }
